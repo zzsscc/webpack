e55c1e7d4e570609ab6c404216ea09739ed4c75a
zhanshichao <zhanshichao@dianwoba.com>
2018-04-28 18:24:38 +0800

    feat(webpack): webpack common merge

--- webpack/webpack.dev.conf.js
+++ webpack/webpack.dev.conf.js
@@ -1,81 +1,15 @@
 const webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const { srcRoot, rootNode } = require('../configs/helpers/path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.conf');
 
-module.exports = {
-  entry: // 入口文件
-    // `${srcRoot('index.js')}`,
-    { // 多文件
-      app: `${srcRoot('index.js')}`,
-      print: `${srcRoot('print.js')}`,
-    },
-  output: {
-    filename: '[name].bundle.js',           // 输出文件
-    path: rootNode('dist'),          // 输出文件的路径
-    // 给url-loader用的资源文件前缀路径,表示资源的发布地址，当配置过该属性后，打包文件中所有通过相对路径引用的资源都会被配置的路径所替换。
-    // 具体表现为：被设置为url-loader的outputPath前的路径  -->  ..path/dist/img
-    publicPath: 'dist/'
-  },
+module.exports = merge(common, {
   devtool: 'inline-source-map',     // 使用 source map，追踪错误和警告位置  // prod 使用source-map
   devServer: {
     // contentBase: './dist',
     hot: true
   },
-  module: {
-    rules: [
-      // 加载样式文件 style-loader  css-loader  sass-loader
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader?importLoaders=2'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader?importLoaders=2',
-          'sass-loader?outputStyle=expanded'
-        ]
-      },
-      // 加载图片 file-loader url-loader
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              outputPath: 'img/',    // 在output中的publicPath路径中的路径
-              limit: 8192     // 小于8129字节的被转为baes64引入，大于8129字节的正常打包在指定的路径下
-            }
-          }
-        ]
-      },
-      // 加载字体文件 file-loader url-loader
-      { test: /\.(woff|woff2)(\?v=\d\.\d\.\d)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
-      { test: /\.ttf(\?v=\d\.\d\.\d)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream' },
-      { test: /\.eot(\?v=\d\.\d\.\d)?$/, loader: 'file-loader' },
-      // 加载资源文件，csv/tsv/xml (json默认支持)
-      {
-        test: /\.(csv|tsv)$/,
-        use: [
-          'csv-loader'
-        ]
-      },
-      {
-        test: /\.xml$/,
-        use: [
-          'xml-loader'
-        ]
-      }
-    ]
-  },
   plugins: [
-    // 打包构建前先清理输出文件夹
-    new CleanWebpackPlugin(['dist'], { root: rootNode('') }),
     new webpack.NamedModulesPlugin(),   // 以便更容易查看要修补(patch)的依赖  // prod不需要
     new webpack.HotModuleReplacementPlugin(),    // prod不需要
-    new UglifyJSPlugin() // dev可以不需要
   ],
-};
+});
